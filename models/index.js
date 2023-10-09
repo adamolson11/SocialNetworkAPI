425ebfc23a80011456d488c275a65456f50dfab7
Adam Olson <adam.lee.olson@outlook.com>
2023-10-09 11:58:51 -0500

    working out bug for index.js of models folder

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const User = require('./User')
+const Thought= require('./Thought')
+
+// In a Node.js and Express.js application, an index.js file is commonly used as the entry point or main file of the application. It serves as the central point where you can import and organize various parts of your application, such as models, routes, middleware, and database connections.
+
+//so in this case it is creating an organized, single entry point for your models folder. you only have two but imagine having a DB with like 50 collections or something. The index would help with keeping it organized. 
+
+
+
+module.exports = {User, Thought}
